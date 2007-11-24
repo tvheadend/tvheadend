c37a5d4b84bfc6eff0337814f61ef20296356436
Andreas Öman <andreas@lonelycoder.com>
2007-11-24 07:06:37 +0000

    disable RTSP for now, it's not access protected

--- http.c
+++ http.c
@@ -62,7 +62,7 @@ static struct strtab HTTP_versiontab[] = {
   { "HTTP/0.9",        HTTP_VERSION_0_9 },
   { "HTTP/1.0",        HTTP_VERSION_1_0 },
   { "HTTP/1.1",        HTTP_VERSION_1_1 },
-  { "RTSP/1.0",        RTSP_VERSION_1_0 },
+  //  { "RTSP/1.0",        RTSP_VERSION_1_0 }, /* not enabled yet */
 };
 
 
