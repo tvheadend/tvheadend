cdf83133f2362849970ef432c62dc1e0fa115d83
Andreas Öman <andreas@lonelycoder.com>
2010-01-17 13:21:26 +0000

    Fix a completly incorrect header

--- src/psi.c
+++ src/psi.c
@@ -1,6 +1,6 @@
 /*
- *  Multicasted IPTV Input
- *  Copyright (C) 2007 Andreas Öman
+ *  MPEG TS Program Specific Information code
+ *  Copyright (C) 2007 - 2010 Andreas Öman
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
