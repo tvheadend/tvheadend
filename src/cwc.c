b45876683120b0f7e7c9a64c94c83e1fee228f2d
Andreas Öman <andreas@lonelycoder.com>
2010-10-08 21:07:09 +0000

    % -> %s

--- src/cwc.c
+++ src/cwc.c
@@ -728,7 +728,7 @@ handle_ecm_reply(cwc_transport_t *ct, ecm_section_t *es, uint8_t *msg,
     
     if(ct->ct_keystate != CT_RESOLVED)
       tvhlog(LOG_INFO, "cwc",
-	     "Obtained key for for service \"%s\" in %lld ms, from %",
+	     "Obtained key for for service \"%s\" in %lld ms, from %s",
 	     t->tht_svcname, delay, ct->ct_cwc->cwc_hostname);
     
     pthread_mutex_lock(&t->tht_stream_mutex);
