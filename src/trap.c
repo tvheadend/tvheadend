f0f956f5f6638ac5febcbd1a315f73e8c7ac80d2
Andreas Öman <andreas@lonelycoder.com>
2011-01-05 04:42:36 +0800

    Add missing inclusion of config.h

--- src/trap.c
+++ src/trap.c
@@ -15,7 +15,7 @@
  *  You should have received a copy of the GNU General Public License
  *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
+#include "config.h"
 #include "trap.h"
 
 char tvh_binshasum[20];
