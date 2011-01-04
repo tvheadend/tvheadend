d66486a3b4c61b02bce3143ee3932b80ec2980cc
Andreas Öman <andreas@lonelycoder.com>
2011-01-05 03:43:09 +0800

    Need to check for ENABLE_LINUXDVB

--- src/service.c
+++ src/service.c
@@ -1060,8 +1060,10 @@ service_get_signal_status(service_t *t, signal_status_t *status)
 {
   // get signal status from the service
   switch(t->s_type) {
+#if ENABLE_LINUXDVB
   case SERVICE_TYPE_DVB:
     return dvb_transport_get_signal_status(t, status);
+#endif
   default:
     return -1;
   }
