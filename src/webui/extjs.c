9ecda62cadc9c1760de8978902f584934aa57b78
jdembski <jdembski@htpc.(none)>
2011-01-05 14:32:00 +0100

    Add config option to delete recordings together with their dvr event

--- src/webui/extjs.c
+++ src/webui/extjs.c
@@ -219,9 +219,6 @@ extjs_tablemgr(http_connection_t *hc, const char *remain, void *opaque)
   const char *op        = http_arg_get(&hc->hc_req_args, "op");
   const char *entries   = http_arg_get(&hc->hc_req_args, "entries");
 
-  if(op == NULL)
-    return 400;
-
   if(tablename == NULL || (dt = dtable_find(tablename)) == NULL)
     return 404;
   
@@ -359,9 +356,6 @@ extjs_channels(http_connection_t *hc, const char *remain, void *opaque)
   const char *op        = http_arg_get(&hc->hc_req_args, "op");
   const char *entries   = http_arg_get(&hc->hc_req_args, "entries");
 
-  if(op == NULL)
-    return 400;
-
   htsmsg_autodtor(in) =
     entries != NULL ? htsmsg_json_deserialize(entries) : NULL;
 
@@ -476,9 +470,6 @@ extjs_xmltv(http_connection_t *hc, const char *remain, void *opaque)
   xmltv_channel_t *xc;
   htsmsg_t *out, *array, *e, *r;
   const char *s;
-
-  if(op == NULL)
-    return 400;
 
   pthread_mutex_lock(&global_lock);
 
@@ -880,6 +871,7 @@ extjs_dvr(http_connection_t *hc, const char *remain, void *opaque)
     htsmsg_add_u32(r, "retention", cfg->dvr_retention_days);
     htsmsg_add_u32(r, "preExtraTime", cfg->dvr_extra_time_pre);
     htsmsg_add_u32(r, "postExtraTime", cfg->dvr_extra_time_post);
+    htsmsg_add_u32(r, "delRec",         !!(cfg->dvr_flags & DVR_DEL_RECORDING));
     htsmsg_add_u32(r, "dayDirs",        !!(cfg->dvr_flags & DVR_DIR_PER_DAY));
     htsmsg_add_u32(r, "channelDirs",    !!(cfg->dvr_flags & DVR_DIR_PER_CHANNEL));
     htsmsg_add_u32(r, "channelInTitle", !!(cfg->dvr_flags & DVR_CHANNEL_IN_TITLE));
@@ -910,6 +902,9 @@ extjs_dvr(http_connection_t *hc, const char *remain, void *opaque)
 
     if((s = http_arg_get(&hc->hc_req_args, "retention")) != NULL)
       dvr_retention_set(cfg,atoi(s));
+    
+   if(http_arg_get(&hc->hc_req_args, "delRec") != NULL)
+      flags |= DVR_DEL_RECORDING;
 
    if((s = http_arg_get(&hc->hc_req_args, "preExtraTime")) != NULL)
      dvr_extra_time_pre_set(cfg,atoi(s));
@@ -1349,9 +1344,6 @@ extjs_iptvservices(http_connection_t *hc, const char *remain, void *opaque)
   const char *entries   = http_arg_get(&hc->hc_req_args, "entries");
   service_t *t, **tvec;
   int count = 0, i = 0;
-
-  if(op == NULL)
-    return 400;
 
   pthread_mutex_lock(&global_lock);
 
