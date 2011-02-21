d7aae26cf15332e10569e790c474bc387552fa1a
Thies Schroeder <mail@thies-schroeder.de>
2011-02-21 17:47:17 +0100

    delete missed events

--- src/dvr/dvr_db.c
+++ src/dvr/dvr_db.c
@@ -1164,7 +1164,9 @@ dvr_val2pri(dvr_prio_t v)
 int
 dvr_entry_delete(dvr_entry_t *de)
 {
-  if(!unlink(de->de_filename) || errno == ENOENT) {
+  if(!unlink(de->de_filename) || 
+	  errno == ENOENT ||
+	  !de->de_filename) {
     tvhlog(LOG_DEBUG, "dvr", "Delete recording '%s'", de->de_filename);
     dvr_entry_remove(de);
     return 0;
