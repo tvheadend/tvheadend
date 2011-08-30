fae2b44333a0d8164bc40a70ff66d3d1164dd062
John Törnblom <john.tornblom@gmail.com>
2011-08-30 18:15:09 +0200

    added property 'dvrId' to the command 'getEvent' that points to the id of a recording.

--- src/htsp.c
+++ src/htsp.c
@@ -712,6 +712,7 @@ htsp_build_event(event_t *e)
 {
   htsmsg_t *out;
   event_t *n;
+  dvr_entry_t *de;
 
   out = htsmsg_create_map();
 
@@ -732,6 +733,10 @@ htsp_build_event(event_t *e)
 
   if(e->e_content_type)
     htsmsg_add_u32(out, "contentType", e->e_content_type);
+
+  if((de = dvr_entry_find_by_event(e)) != NULL) {
+    htsmsg_add_u32(out, "dvrId", de->de_id);
+  }
 
   n = RB_NEXT(e, e_channel_link);
   if(n != NULL)
