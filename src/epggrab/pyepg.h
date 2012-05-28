e4f4411777153e10acae72d9a87a61d48e6a8efd
Adam Sutton <dev@adamsutton.me.uk>
2012-05-28 16:43:42 +0100

    Some tidying up of epg grabbers and added stubs for missing functions in epg.

--- src/epggrab/pyepg.h
+++ src/epggrab/pyepg.h
@@ -1,11 +1,25 @@
 /*
- * PyEPG grabber module
+ *  Electronic Program Guide - pyepg grabber
+ *  Copyright (C) 2012 Adam Sutton
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-
-#include "epggrab.h"
 
 #ifndef EPGGRAB_PYEPG_H
 #define EPGGRAB_PYEPG_H
+
+#include "epggrab.h"
 
 epggrab_module_t* pyepg_init ( void );
 
