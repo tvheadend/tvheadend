16d5d26ef37253f01fa9afa28faa625e943dd96c
Adam Sutton <dev@adamsutton.me.uk>
2012-06-25 09:59:32 +0100

    Keep EPG related TDMIs in the EPG queue. Decision about wether to expend any effort doing the grabbing will be done within the grabber itself. This should be a better solution.

--- src/dvb/dvb_fe.c
+++ src/dvb/dvb_fe.c
@@ -229,15 +229,9 @@ dvb_fe_stop(th_dvb_mux_instance_t *tdmi)
 
   if(tdmi->tdmi_enabled) {
     int ti;
-    time_t now;
-    epggrab_ota_mux_t *ota;
-    time(&now);
-    LIST_FOREACH(ota, &tdmi->tdmi_epg_grabbers, tdmi_link) {
-      if ( now >= (ota->completed+ota->interval) ) break;
-    }
-    ti = ota                       ? TDA_SCANQ_EPG
-       : tdmi->tdmi_quality == 100 ? TDA_SCANQ_OK
-                                   : TDA_SCANQ_BAD;
+    ti = LIST_FIRST(&tdmi->tdmi_epg_grabbers) ? TDA_SCANQ_EPG
+       : tdmi->tdmi_quality == 100            ? TDA_SCANQ_OK
+                                              : TDA_SCANQ_BAD;
     tdmi->tdmi_scan_queue = &tda->tda_scan_queues[ti];
     TAILQ_INSERT_TAIL(tdmi->tdmi_scan_queue, tdmi, tdmi_scan_link);
   }
