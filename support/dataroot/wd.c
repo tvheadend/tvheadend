a5d44a7dae4e9f688daf1ce2cd7e6c6287fdab37
Adam Sutton <dev@adamsutton.me.uk>
2012-06-26 13:09:28 +0100

    Minor mod to wd and hts settings to allow abs paths to be passed to hts_settings_load().

--- support/dataroot/wd.c
+++ support/dataroot/wd.c
@@ -1,4 +1,14 @@
+#include <string.h>
+#include <unistd.h>
+#include <assert.h>
+
 const char *tvheadend_dataroot(void)
 {
-  return "./";
+  static char cwd[256] = { 0 };
+  if (!*cwd) {
+    assert(getcwd(cwd, 254));
+    strcat(cwd, "/");
+  }
+   printf("cwd = %s\n", cwd);
+  return cwd;
 }
