9a58b7ab7602970aca698b2cac9a1f18d71cbdd6
Adam Sutton <dev@adamsutton.me.uk>
2012-08-17 20:30:25 +0100

    Fix mistake in new configure script for using make install. Relates #1127.

--- support/posix.mk
+++ support/posix.mk
@@ -4,13 +4,13 @@ ICON = support/gnome/tvheadend.svg
 INSTICON= ${DESTDIR}$(prefix)/share/icons/hicolor/scalable/apps
 
 
-install: ${PROG}.datadir ${MAN}
-	install -D ${PROG}.datadir ${bindir}/tvheadend
+install: ${PROG} ${MAN}
+	install -D ${PROG} ${bindir}/tvheadend
 	install -D ${MAN} ${mandir}/tvheadend.1
 
 	for bundle in ${BUNDLES}; do \
-		mkdir -p ${datadir}/$$bundle ;\
-		cp -r $$bundle/*  ${datadir}/$$bundle ;\
+		mkdir -p ${datadir}/tvheadend/$$bundle ;\
+		cp -r $$bundle/*  ${datadir}/tvheadend/$$bundle ;\
 	done
 
 
