f1978b8108132fa75e1e4624a4c409bcb3989e34
John Törnblom <john.tornblom@gmail.com>
2012-08-21 23:20:43 +0200

    added eac3 support to the pmt builder and audio detection macro.

--- src/tvheadend.h
+++ src/tvheadend.h
@@ -169,7 +169,8 @@ typedef enum {
 
 #define SCT_ISVIDEO(t) ((t) == SCT_MPEG2VIDEO || (t) == SCT_H264)
 #define SCT_ISAUDIO(t) ((t) == SCT_MPEG2AUDIO || (t) == SCT_AC3 || \
-                        (t) == SCT_AAC || (t) == SCT_MP4A)
+                        (t) == SCT_AAC || (t) == SCT_MP4A ||	   \
+			(t) == SCT_EAC3)
 #define SCT_ISSUBTITLE(t) ((t) == SCT_TEXTSUB || (t) == SCT_DVBSUB)
 
 /**
