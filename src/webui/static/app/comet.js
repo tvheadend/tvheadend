0e47d6c5eccd971b80e33782ae3d1e523cd8b676
xhaggi <hagman@gmx.de>
2012-09-06 20:43:59 +0200

    webui: button 'Reset' and 'Add DVB Network by location' now works like expected cosmetics: format all js files

--- src/webui/static/app/comet.js
+++ src/webui/static/app/comet.js
@@ -2,19 +2,19 @@
  * Comet interfaces
  */
 Ext.extend(tvheadend.Comet = function() {
-    this.addEvents({
-	accessUpdate: true,
-	tvAdapter: true,
-	dvbMux: true,
-	dvbStore: true,
-	dvbSatConf: true,
-	logmessage: true,
-	channeltags: true,
-	autorec: true,
-	dvrdb: true,
-        dvrconfig: true,
-	channels: true
-    });
+	this.addEvents({
+		accessUpdate : true,
+		tvAdapter : true,
+		dvbMux : true,
+		dvbStore : true,
+		dvbSatConf : true,
+		logmessage : true,
+		channeltags : true,
+		autorec : true,
+		dvrdb : true,
+		dvrconfig : true,
+		channels : true
+	});
 }, Ext.util.Observable);
 
 tvheadend.comet = new tvheadend.Comet();
@@ -22,49 +22,49 @@ tvheadend.boxid = null;
 
 tvheadend.cometPoller = function() {
 
-    var failures = 0;
+	var failures = 0;
 
-    var cometRequest = new Ext.util.DelayedTask(function() {
+	var cometRequest = new Ext.util.DelayedTask(function() {
 
-	Ext.Ajax.request({
-	    url: 'comet/poll',
-	    params : { 
-		boxid: (tvheadend.boxid ? tvheadend.boxid : null),
-		immediate: failures > 0 ? 1 : 0
-	    },
-	    success: function(result, request) { 
-		parse_comet_response(result.responseText);
+		Ext.Ajax
+			.request({
+				url : 'comet/poll',
+				params : {
+					boxid : (tvheadend.boxid ? tvheadend.boxid : null),
+					immediate : failures > 0 ? 1 : 0
+				},
+				success : function(result, request) {
+					parse_comet_response(result.responseText);
 
-		if(failures > 1) {
-		    tvheadend.log('Reconnected to Tvheadend',
-				  'font-weight: bold; color: #080');
-		}
-		failures = 0;
-	    },
-	    failure: function(result, request) { 
-		cometRequest.delay(failures ? 1000 : 1);
-		if(failures == 1) {
-		    tvheadend.log('There seems to be a problem with the ' + 
-				  'live update feed from Tvheadend. ' +
-				  'Trying to reconnect...',
-				  'font-weight: bold; color: #f00');
-		}
-		failures++;
-	    }
+					if (failures > 1) {
+						tvheadend.log('Reconnected to Tvheadend',
+							'font-weight: bold; color: #080');
+					}
+					failures = 0;
+				},
+				failure : function(result, request) {
+					cometRequest.delay(failures ? 1000 : 1);
+					if (failures == 1) {
+						tvheadend.log('There seems to be a problem with the '
+							+ 'live update feed from Tvheadend. '
+							+ 'Trying to reconnect...',
+							'font-weight: bold; color: #f00');
+					}
+					failures++;
+				}
+			});
 	});
-    });
 
-    function parse_comet_response(responsetxt) {
-	response = Ext.util.JSON.decode(responsetxt);
-	tvheadend.boxid = response.boxid
-	for(x = 0; x < response.messages.length; x++) {
-            m = response.messages[x];
-	    tvheadend.comet.fireEvent(m.notificationClass, m);
+	function parse_comet_response(responsetxt) {
+		response = Ext.util.JSON.decode(responsetxt);
+		tvheadend.boxid = response.boxid
+		for (x = 0; x < response.messages.length; x++) {
+			m = response.messages[x];
+			tvheadend.comet.fireEvent(m.notificationClass, m);
+		}
+		cometRequest.delay(100);
 	}
+	;
+
 	cometRequest.delay(100);
-    };
-
-    cometRequest.delay(100);
 }
-
-
