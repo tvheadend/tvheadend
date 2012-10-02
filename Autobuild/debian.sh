90c5b5838949d6b1382f8e7e4ad0075a24e55c50
Adam Sutton <dev@adamsutton.me.uk>
2012-10-02 12:13:36 +0100

    Updated the build system for better consistency. Also make it easier to do source builds for things like launchpad PPAs.

--- Autobuild/debian.sh
+++ Autobuild/debian.sh
@@ -1,16 +1,12 @@
 BUILD_DEPS=`awk 'BEGIN {cnt = 1;} /^Build-Depends:/ {split($0, line, ":");split(line[2], deps, ",");for (i in deps) {d = deps[i];sub(/^ */, "", d);sub(/ *$/, "", d);split(d, tokens, " ");packages[cnt] = tokens[1];cnt++;}} END {out = ""; for(i = 1; i <= cnt; i++) {out = out packages[i] " ";} print out; }' debian/control`
 CHANGELOG=debian/changelog
 NOW=`date -R`
-VER=`git describe | sed "s/\([0-9]*\)\.\([0-9]*\)-\([0-9]*\)-.*/\1.\2.\3/"`
+VER=`$(dirname $0)/support/version`
+[ -z "${DEBDIST:-}" ] && DEBDIST=""
 
 build() 
 {
-    echo >${CHANGELOG} "tvheadend (${VER}) unstable; urgency=low"
-    echo >>${CHANGELOG}
-    echo >>${CHANGELOG} "  * The full changelog can be found at "
-    echo >>${CHANGELOG} "    http://www.lonelycoder.com/tvheadend/download"
-    echo >>${CHANGELOG}
-    echo >>${CHANGELOG} " -- Andreas Öman <andreas@lonelycoder.com>  ${NOW}"
+    $(dirname $0)/support/changelog "$CHANGELOG" "$DEBDIST" "$VER"
     
     export JOBSARGS
     export JARGS
@@ -18,22 +14,22 @@ build()
     dpkg-buildpackage -b -us -uc
 
     for a in ../tvheadend*${VER}*.deb; do
-	versioned_artifact "$a" deb application/x-deb `basename $a`
+        versioned_artifact "$a" deb application/x-deb `basename $a`
     done
 
     for a in ../tvheadend*${VER}*.changes; do
-	versioned_artifact "$a" changes text/plain `basename $a`
+        versioned_artifact "$a" changes text/plain `basename $a`
     done
 }
 
 clean() 
 {
     for a in ../tvheadend*${VER}*.deb; do
-	rm -f "$a"
+        rm -f "$a"
     done
 
     for a in ../tvheadend*${VER}*.changes; do
-	rm -f "$a"
+        rm -f "$a"
     done
 
     rm -f ${CHANGELOG}
@@ -43,8 +39,8 @@ clean()
 deps() 
 {
     if [[ $EUID -ne 0 ]]; then
-	echo "Build dependencies must be installed as root"
-	exit 1
+        echo "Build dependencies must be installed as root"
+        exit 1
     fi
     apt-get -y install ${BUILD_DEPS}
 }
