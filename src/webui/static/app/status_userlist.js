7c45ee0d5f84a65f3cb10c05e1a4cb4e6fac1df8
xhaggi <sascha.woo@gmail.com>
2012-10-09 15:26:29 +0200

    fixed: wrong parameter on constructor tvheadend.tableEditor

--- src/webui/static/app/status_userlist.js
+++ src/webui/static/app/status_userlist.js
@@ -1,81 +1,82 @@
 tvheadend.storeStatusUserlist = new Ext.data.JsonStore({
-        root : 'entries',
-        id : 'id',
-        url : '/status/userlist',
-        baseParams : {
-                op : 'streamdata'
-        },
-        fields : [ 'id', 'username', 'startlog', 'currlog', 'ip', 'type', 'streamdata' ],
-        autoLoad : true
+	root : 'entries',
+	id : 'id',
+	url : '/status/userlist',
+	baseParams : {
+		op : 'streamdata'
+	},
+	fields : [ 'id', 'username', 'startlog', 'currlog', 'ip', 'type',
+			'streamdata' ],
+	autoLoad : true
 });
-
 tvheadend.storeStatusUserlist.setDefaultSort('username', 'ASC');
 tvheadend.comet.on('statususerlist', function(m) {
-        if (m.reload != null) tvheadend.storeStatusUserlist.reload();
+	if (m.reload != null)
+		tvheadend.storeStatusUserlist.reload();
 });
 
 tvheadend.statususerlist = function() {
 	var fm = Ext.form;
 
 	var cm = new Ext.grid.ColumnModel({
-  defaultSortable: true,
-  columns : [ {
-                header : "ID",
-                dataIndex : 'id',
-		width : 10,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-        }, {
-                header : "Username",
-                dataIndex : 'username',
-		width : 30,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-        }, {
-                header : "Startlog",
-                dataIndex : 'startlog',
-		width : 40,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-        }, {
-                header : "Currlog",
-                dataIndex : 'currlog',
-		width : 40,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-        }, {
-                header : "IP",
-                dataIndex : 'ip',
-		width : 40,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-        }, {
-                header : "Type",
-                dataIndex : 'type',
-		width : 40,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-        }, {
-                header : "Streamdata",
-                dataIndex : 'streamdata',
-		width : 40,
-                editor : new fm.TextField({
-                        allowBlank : false
-                })
-	} ]});
+		defaultSortable : true,
+		columns : [ {
+			header : "ID",
+			dataIndex : 'id',
+			width : 10,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		}, {
+			header : "Username",
+			dataIndex : 'username',
+			width : 30,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		}, {
+			header : "Startlog",
+			dataIndex : 'startlog',
+			width : 40,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		}, {
+			header : "Currlog",
+			dataIndex : 'currlog',
+			width : 40,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		}, {
+			header : "IP",
+			dataIndex : 'ip',
+			width : 40,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		}, {
+			header : "Type",
+			dataIndex : 'type',
+			width : 40,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		}, {
+			header : "Streamdata",
+			dataIndex : 'streamdata',
+			width : 40,
+			editor : new fm.TextField({
+				allowBlank : false
+			})
+		} ]
+	});
 
 	var UserStatusRecord = Ext.data.Record.create([ 'id', 'username',
-		'startlog', 'currlog', 'ip', 'type', 'streamdata' ]);
+			'startlog', 'currlog', 'ip', 'type', 'streamdata' ]);
 
 	return new tvheadend.tableEditor('Status', 'statususerlist', cm,
-		UserStatusRecord,
-		[ 'id', 'username', 'startlog', 'currlog', 'ip', 'type', 'streamdata' ],
-		tvheadend.servicetypeStore, 'status_userlist.html', 'group');
+			UserStatusRecord, null, tvheadend.storeStatusUserlist,
+			'config_access.html', 'group');
 
 }
