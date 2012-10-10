1cc67b75e8f7a90ef0d6aaaf88cf5cf633c8c840
Adam Sutton <dev@adamsutton.me.uk>
2012-10-10 12:52:07 +0100

    Add the concept of onid into the DVB structures, this will help overcome a few minor issues.

--- src/dvb/dvb_preconf.c
+++ src/dvb/dvb_preconf.c
@@ -98,7 +98,7 @@ dvb_mux_preconf_add(th_dvb_adapter_t *tda, const network_t *net,
 
     dmc.dmc_satconf = dvb_satconf_entry_find(tda, satconf, 0);
       
-    dvb_mux_create(tda, &dmc, 0xffff, NULL, source, 1, 1, NULL, NULL);
+    dvb_mux_create(tda, &dmc, 0, 0xffff, NULL, source, 1, 1, NULL, NULL);
   }
 }
 
