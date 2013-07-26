6f8b04efa9bb242366b88f12fd340f23b1f565ef
Bernhard Froehlich <decke@bluelife.at>
2013-07-26 17:36:18 +0200

    tvhpoll: Fix building on FreeBSD i386
    src/tvhpoll.c: In function 'tvhpoll_add':
    src/tvhpoll.c:124: warning: cast to pointer from integer of different size
    src/tvhpoll.c:127: warning: cast to pointer from integer of different size
    src/tvhpoll.c: In function 'tvhpoll_wait':
    src/tvhpoll.c:180: warning: cast from pointer to integer of different size

--- src/tvhpoll.c
+++ src/tvhpoll.c
@@ -121,10 +121,10 @@ int tvhpoll_add
   tvhpoll_alloc(tp, num);
   for (i = 0; i < num; i++) {
     if (evs[i].events & TVHPOLL_OUT){
-      EV_SET(tp->ev+i, evs[i].fd, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, (void*)evs[i].data.u64);
+      EV_SET(tp->ev+i, evs[i].fd, EVFILT_WRITE, EV_ADD | EV_ENABLE, 0, 0, (intptr_t*)evs[i].data.u64);
     }
     if (evs[i].events & TVHPOLL_IN){
-      EV_SET(tp->ev+i, evs[i].fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, (void*)evs[i].data.u64);
+      EV_SET(tp->ev+i, evs[i].fd, EVFILT_READ, EV_ADD | EV_ENABLE, 0, 0, (intptr_t*)evs[i].data.u64);
     }
   }
   return kevent(tp->fd, tp->ev, num, NULL, 0, NULL);
@@ -177,7 +177,7 @@ int tvhpoll_wait
   for (i = 0; i < nfds; i++) {
     evs[i].fd       = tp->ev[i].ident;
     evs[i].events   = 0;
-    evs[i].data.u64 = (uint64_t)tp->ev[i].udata;
+    evs[i].data.u64 = (intptr_t)tp->ev[i].udata;
     if (tp->ev[i].filter & EVFILT_WRITE) evs[i].events |= TVHPOLL_OUT;
     if (tp->ev[i].filter & EVFILT_READ)  evs[i].events |= TVHPOLL_IN;
     if (tp->ev[i].flags  & EV_ERROR)     evs[i].events |= TVHPOLL_ERR;
