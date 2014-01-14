c5d504995ee534b4d1f519146092aed8079152a6
Dietmar Konermann <dk@diddle-online.de>
2014-01-14 21:09:24 +0100

    Changed date format in epg view

--- src/webui/static/app/epg.js
+++ src/webui/static/app/epg.js
@@ -250,13 +250,6 @@ tvheadend.epg = function() {
 		setMetaAttr(meta, record);
 
 		var dt = new Date(value);
-		return dt.format('l H:i');
-	}
-
-	function renderDateFull(value, meta, record, rowIndex, colIndex, store) {
-		setMetaAttr(meta, record);
-
-		var dt = new Date(value);
 		return dt.format('D, M d, H:i');
 	}
 
@@ -317,24 +310,11 @@ tvheadend.epg = function() {
 		renderer : renderDate
 	}, {
 		width : 100,
-		id : 'lstart',
-		header : "Start (long)",
-		dataIndex : 'start',
-		renderer : renderDateFull
-	}, {
-		width : 100,
 		hidden : true,
 		id : 'end',
 		header : "End",
 		dataIndex : 'end',
 		renderer : renderDate
-	}, {
-		width : 100,
-		hidden : true,
-		id : 'lend',
-		header : "End (long)",
-		dataIndex : 'end',
-		renderer : renderDateFull
 	}, {
 		width : 100,
 		id : 'duration',
