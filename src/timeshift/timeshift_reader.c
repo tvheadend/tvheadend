c56ca45ab7eee8bf5ce6526ad2da55fadd9c8fde
Adam Sutton <dev@adamsutton.me.uk>
2014-03-04 15:42:49 +0000

    timeshift debug: correct format string

--- src/timeshift/timeshift_reader.c
+++ src/timeshift/timeshift_reader.c
@@ -735,7 +735,7 @@ void *timeshift_reader ( void *p )
         th_pkt_t *pkt = sm->sm_data;
         tvhtrace("timeshift",
                  "ts %d pkt out - stream %d type %c pts %10"PRId64
-                 " dts %10"PRId64 " dur %10d len %"PRIsize_t" time %"PRItime_t,
+                 " dts %10"PRId64 " dur %10d len %"PRIsize_t" time %"PRId64,
                  ts->id,
                  pkt->pkt_componentindex,
                  pkt_frametype_to_char(pkt->pkt_frametype),
