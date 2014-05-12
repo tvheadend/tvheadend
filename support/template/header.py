8a2e18453a4a3d67c0f831fa02e5d4527ccfe9d4
Adam Sutton <dev@adamsutton.me.uk>
2014-05-12 21:18:57 +0100

    license: create various headers

---
+++ support/template/header.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2008-2014 Tvheadend Foundation CIC
+#
+# This file is part of Tvheadend
+#
+# Tvheadend is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Tvheadend is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Tvheadend.  If not, see <http://www.gnu.org/licenses/>.
+#
+# For more details, including opportunities for alternative licensing,
+# please read the LICENSE file.
+#
+# ############################################################################
+
+"""
+Module Title
+
+Module Decsription
+"""
+
+# ############################################################################
+# Editor Configuration
+#
+# vim:sts=2:ts=2:sw=2:et
+# ############################################################################
