29cb197a04f0331b59f889fe1033fd7cf0ff66c4
Jaroslav Kysela <perex@perex.cz>
2014-06-23 15:39:19 +0200

    mpegts_input: Fix the PID open (filtered components)

--- src/input/mpegts/mpegts_input.c
+++ src/input/mpegts/mpegts_input.c
@@ -317,7 +317,7 @@ mpegts_input_open_service ( mpegts_input_t *mi, mpegts_service_t *s, int init )
   mi->mi_open_pid(mi, s->s_dvb_mux, s->s_pmt_pid, MPS_STREAM, s);
   mi->mi_open_pid(mi, s->s_dvb_mux, s->s_pcr_pid, MPS_STREAM, s);
   /* Open only filtered components here */
-  TAILQ_FOREACH(st, &s->s_filt_components, es_link) {
+  TAILQ_FOREACH(st, &s->s_filt_components, es_filt_link) {
     if (st->es_type != SCT_CA)
       mi->mi_open_pid(mi, s->s_dvb_mux, st->es_pid, MPS_STREAM, s);
   }
