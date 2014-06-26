2bdff4c0ece85666019ce90f22a5bb0e6e29e677
Jaroslav Kysela <perex@perex.cz>
2014-06-26 21:05:05 +0200

    muxer passthrough: EPIPE is the end-of-streaming notification

--- src/muxer/muxer_pass.c
+++ src/muxer/muxer_pass.c
@@ -401,6 +401,8 @@ pass_muxer_write(muxer_t *m, const void *data, size_t size)
     pm->m_errors++;
   } else if(tvh_write(pm->pm_fd, data, size)) {
     pm->pm_error = errno;
+    if (errno == EPIPE) /* this is an end-of-streaming notification */
+      return;
     tvhlog(LOG_ERR, "pass", "%s: Write failed -- %s", pm->pm_filename, 
 	   strerror(errno));
     m->m_errors++;
