6088e2aaf039262932a61ad77ce570f516d6eefd
sangood <zhangsangood@gmail.com>
2014-07-19 11:51:13 +0800

    Update dvb_psi.c
    fixed missing ac3 audio stream

--- src/input/mpegts/dvb_psi.c
+++ src/input/mpegts/dvb_psi.c
@@ -1352,7 +1352,9 @@ psi_parse_pmt
     case 0x81:
       hts_stream_type = SCT_AC3;
       break;
-    
+    case 0x06:
+      hts_stream_type = SCT_AC3;
+      break;
     case 0x0f:
       hts_stream_type = SCT_MP4A;
       break;
