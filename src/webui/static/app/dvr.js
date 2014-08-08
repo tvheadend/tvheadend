fa326c143eddb73a9400b4d4fa99389650f93e5c
Sam Stenvall <neggelandia@gmail.com>
2014-08-08 09:42:14 +0300

    [webui] make the DVR grids stateful (remember column positions/visibility)

--- src/webui/static/app/dvr.js
+++ src/webui/static/app/dvr.js
@@ -565,6 +565,8 @@ tvheadend.dvrschedule = function(title, iconCls, dvrStore) {
     ]);
 
     var panel = new Ext.grid.GridPanel({
+        stateful: true,
+        stateId: dvrStore.url,
         loadMask: true,
         stripeRows: true,
         disableSelection: false,
