4a13a2b1babc2de9ded5bed9a8e8f6740a32cd7e
Jaroslav Kysela <perex@perex.cz>
2014-08-18 11:46:38 +0200

    uuid: check the uuid string size

--- src/uuid.c
+++ src/uuid.c
@@ -107,6 +107,10 @@ uuid_init_bin ( tvh_uuid_t *u, const char *str )
 {
   memset(u, 0, sizeof(tvh_uuid_t));
   if (str) {
+    if (strlen(str) != UUID_HEX_SIZE - 1) {
+      tvherror("uuid", "wrong uuid size");
+      exit(1);
+    }
     return hex2bin(u->bin, sizeof(u->bin), str);
   } else if (read(fd, u->bin, sizeof(u->bin)) != sizeof(u->bin)) {
     tvherror("uuid", "failed to read from %s", RANDOM_PATH);
