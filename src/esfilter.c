f7b4d108330928d7f2b1cb785096369f2f0bdc5d
Jaroslav Kysela <perex@perex.cz>
2014-09-01 14:17:02 +0200

    channel: cleanup the configuration tree
      <cfg>/channel/config
      <cfg>/channel/tag
    
    Also use hts_settings_load() instead hts_settings_load_r() for
    the appropriate places..

--- src/esfilter.c
+++ src/esfilter.c
@@ -1034,7 +1034,7 @@ esfilter_init(void)
   for (i = 0; i <= ESF_CLASS_LAST; i++)
     TAILQ_INIT(&esfilters[i]);
 
-  if (!(c = hts_settings_load_r(1, "esfilter")))
+  if (!(c = hts_settings_load("esfilter")))
     return;
   HTSMSG_FOREACH(f, c) {
     if (!(e = htsmsg_field_get_map(f)))
