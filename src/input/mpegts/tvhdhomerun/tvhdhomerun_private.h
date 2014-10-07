4a4409ee96a519a09185cbaaa7c3eabe08c38e13
Beralt <beralt@beralt.nl>
2014-10-07 14:39:15 +0200

    tvhdhomerun: disable debug mutex tracking

--- src/input/mpegts/tvhdhomerun/tvhdhomerun_private.h
+++ src/input/mpegts/tvhdhomerun/tvhdhomerun_private.h
@@ -34,7 +34,7 @@ typedef struct tvhdhomerun_frontend    tvhdhomerun_frontend_t;
 static struct hdhomerun_debug_t* hdhomerun_debug_obj = 0;
 
 // Defines for debugging locks...
-#if 1
+#if 0
 
 #define PTHREAD_MUTEX_LOCK(x) \
         tvhdebug("tvhdhomerun", "lock "#x": %s:%d", __FUNCTION__,__LINE__); \
