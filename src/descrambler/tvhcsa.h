2f13317fa0316d1246102a8bbfe971460b8d7904
Jaroslav Kysela <perex@perex.cz>
2015-01-27 19:55:37 +0100

    tvhcsa: fix compilation when TVHCSA is not enabled, fixes #2642

--- src/descrambler/tvhcsa.h
+++ src/descrambler/tvhcsa.h
@@ -64,6 +64,8 @@ typedef struct tvhcsa
   
 } tvhcsa_t;
 
+#if ENABLE_TVHCSA
+
 int  tvhcsa_set_type( tvhcsa_t *csa, int type );
 
 void tvhcsa_set_key_even( tvhcsa_t *csa, const uint8_t *even );
@@ -71,5 +73,17 @@ void tvhcsa_set_key_odd ( tvhcsa_t *csa, const uint8_t *odd );
 
 void tvhcsa_init    ( tvhcsa_t *csa );
 void tvhcsa_destroy ( tvhcsa_t *csa );
+
+#else
+
+static inline int tvhcsa_set_type( tvhcsa_t *csa, int type ) { return -1; }
+
+static inline void tvhcsa_set_key_even( tvhcsa_t *csa, const uint8_t *even ) { };
+static inline void tvhcsa_set_key_odd ( tvhcsa_t *csa, const uint8_t *odd ) { };
+
+static inline void tvhcsa_init ( tvhcsa_t *csa ) { };
+static inline void tvhcsa_destroy ( tvhcsa_t *csa ) { };
+
+#endif
 
 #endif /* __TVH_CSA_H__ */
