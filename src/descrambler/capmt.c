ae281c97e6d9ff6c63e641fdfff9d36752766e24
Jaroslav Kysela <perex@perex.cz>
2015-05-11 16:44:17 +0200

    capmt: add log for filter match

--- src/descrambler/capmt.c
+++ src/descrambler/capmt.c
@@ -1718,11 +1718,13 @@ capmt_table_input(void *opaque, int pid, const uint8_t *data, int len, int emm)
           if ((data[i + 2] & f->mask[i]) != f->filter[i])
             break;
         }
-        if (i >= DMX_FILTER_SIZE && i + 2 <= len)
+        if (i >= DMX_FILTER_SIZE && i + 2 <= len) {
+          tvhtrace("capmt", "filter match pid %d len %d emm %d", pid, len, emm);
           capmt_filter_data(capmt,
                             o->adapter, demux_index,
                             filter_index, data, len,
                             cf->dmx[filter_index].flags);
+        }
       }
     }
   }
