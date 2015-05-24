247dd3a09f0f7c84df858e27e71f0706d50ccbe5
Jaroslav Kysela <perex@perex.cz>
2015-05-24 20:52:08 +0200

    css.py: fix PWD usage

--- support/css.py
+++ support/css.py
@@ -7,6 +7,7 @@ import sys, os
 
 VERBOSE = 'V' in os.environ and len(os.environ['V']) > 0
 TVHDIR = os.path.realpath('.')
+PWD = os.path.realpath(os.environ['PWD'])
 
 def info(fmt, *msg):
   sys.stderr.write(" [INFO ] " + (fmt % msg) + '\n')
@@ -37,7 +38,7 @@ def url(fn):
 
   f = utf8open(fn, 'r')
   if fn[0] != '/':
-    fn = os.path.join(os.environ['PWD'], fn)
+    fn = os.path.join(PWD, fn)
   fn = os.path.normpath(fn)
   if VERBOSE:
     info('css: %s', fn)
@@ -77,7 +78,7 @@ def url(fn):
 def utf_check(fn):
   f = utf8open(fn, 'r')
   if fn[0] != '/':
-    fn = os.path.join(os.environ['PWD'], fn)
+    fn = os.path.join(PWD, fn)
   fn = os.path.normpath(fn)
   if VERBOSE:
     info('utf-check: %s', fn)
