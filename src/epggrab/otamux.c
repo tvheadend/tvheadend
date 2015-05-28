84c9f65c80dd70d2354ca116cbcf671124027289
Jaroslav Kysela <perex@perex.cz>
2015-05-28 08:35:19 +0200

    epggrab: ota - fix grabbing, fixes #2900

--- src/epggrab/otamux.c
+++ src/epggrab/otamux.c
@@ -813,6 +813,8 @@ epggrab_ota_init ( void )
   if (!lstat(path, &st))
     if (!S_ISDIR(st.st_mode))
       hts_settings_remove("epggrab/otamux");
+
+  epggrab_ota_running = 1;
   
   /* Load config */
   if ((c = hts_settings_load_r(1, "epggrab/otamux"))) {
@@ -822,8 +824,6 @@ epggrab_ota_init ( void )
     }
     htsmsg_destroy(c);
   }
-
-  epggrab_ota_running = 1;
 }
 
 void
