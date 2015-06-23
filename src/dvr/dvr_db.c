aae9d6067b798362f462f5a0e171ad2de20a07c2
Jaroslav Kysela <perex@perex.cz>
2015-06-23 16:10:42 +0200

    DVR: save 'don't reschedule' flag to the config

--- src/dvr/dvr_db.c
+++ src/dvr/dvr_db.c
@@ -2213,8 +2213,7 @@ const idclass_t dvr_entry_class = {
       .type     = PT_BOOL,
       .id       = "noresched",
       .name     = N_("Don't Reschedule"),
-      .off      = offsetof(dvr_entry_t, de_dont_reschedule),
-      .opts     = PO_RDONLY,
+      .off      = offsetof(dvr_entry_t, de_dont_reschedule)
     },
     {
       .type     = PT_STR,
