759222c3b759387de2fed6f283f8b3dc55e1d899
BenEfrati <ben1189@gmail.com>
2015-09-04 23:56:33 +0300

    Update mpegts_mux.c

--- src/input/mpegts/mpegts_mux.c
+++ src/input/mpegts/mpegts_mux.c
@@ -30,31 +30,6 @@
 #include <assert.h>
 
 static void mpegts_mux_scan_timeout ( void *p );
-int
-pthread_mutex_timedlock 
-  ( pthread_mutex_t *mutex, struct timespec *timeout )
-{
- struct timeval timenow;
- struct timespec sleepytime;
- int retcode;
- 
- /* This is just to avoid a completely busy wait */
- sleepytime.tv_sec = 0;
- sleepytime.tv_nsec = 10000000; /* 10ms */
- 
- while ((retcode = pthread_mutex_trylock (mutex)) == EBUSY) {
-  gettimeofday (&timenow, NULL);
-  
-  if (timenow.tv_sec >= timeout->tv_sec &&
-      (timenow.tv_usec * 1000) >= timeout->tv_nsec) {
-   return ETIMEDOUT;
-}
-  
-  nanosleep (&sleepytime, NULL);
- }
- 
- return retcode;
-}
 /* ****************************************************************************
  * Mux instance (input linkage)
  * ***************************************************************************/
