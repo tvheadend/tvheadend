8343aa948d57f4d0f616c7dc943f3394cd11c918
Jaroslav Kysela <perex@perex.cz>
2015-09-22 12:42:38 +0200

    move android specific code from tvheadend.h to compat.h

--- src/tvheadend.h
+++ src/tvheadend.h
@@ -51,24 +51,7 @@
 
 #define ERRNO_AGAIN(e) ((e) == EAGAIN || (e) == EINTR || (e) == EWOULDBLOCK)
 
-#if ENABLE_ANDROID
-#define S_IEXEC S_IXUSR
-#define epoll_create1(EPOLL_CLOEXEC) epoll_create(n)
-#define inotify_init1(IN_CLOEXEC) inotify_init()
-#include <time64.h>
-// 32-bit Android has only timegm64() and not timegm().
-// We replicate the behaviour of timegm() when the result overflows time_t.
-static inline time_t timegm(struct tm* const t);
-time_t timegm(struct tm* const t) {
-  // time_t is signed on Android.
-  static const time_t kTimeMax = ~(1L << (sizeof(time_t) * CHAR_BIT - 1));
-  static const time_t kTimeMin = (1L << (sizeof(time_t) * CHAR_BIT - 1));
-  time64_t result = timegm64(t);
-  if (result < kTimeMin || result > kTimeMax)
-    return -1;
-  return result;
-}
-#endif
+#include "compat.h"
 
 typedef struct {
   const char     *name;
@@ -784,4 +767,4 @@ void tvh_qsort_r(void *base, size_t nmemb, size_t size, int (*compar)(const void
 #define PRItime_t       "ld"
 #endif
 
-#endif /* TV_HEAD_H */
+#endif /* TVHEADEND_H */
