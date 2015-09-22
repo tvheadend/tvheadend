e3e8a56d7626942b364d7107f8d7a8b32ebaa956
Jaroslav Kysela <perex@perex.cz>
2015-09-22 16:29:25 +0200

    profile: modify resolution label for transcoding to 'resolution (height)'

--- src/profile.c
+++ src/profile.c
@@ -1490,7 +1490,7 @@ const idclass_t profile_transcode_class =
     {
       .type     = PT_U32,
       .id       = "resolution",
-      .name     = "Resolution",
+      .name     = "Resolution (height)",
       .off      = offsetof(profile_transcode_t, pro_resolution),
       .def.u32  = 384,
     },
