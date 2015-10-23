061a04dcd03a1a77fc8e2af82e473b4d7ac2e37f
Jaroslav Kysela <perex@perex.cz>
2015-10-23 21:04:49 +0200

    linuxdvb: satconf - fix wrong udelay (milli/micro bug)

--- src/input/mpegts/linuxdvb/linuxdvb_satconf.c
+++ src/input/mpegts/linuxdvb/linuxdvb_satconf.c
@@ -713,7 +713,7 @@ linuxdvb_satconf_start ( linuxdvb_satconf_t *ls, int delay, int vol )
     ls->ls_last_tone_off = 1;
   }
   if (delay)
-    usleep(delay);
+    usleep(delay*1000);
   return 0;
 }
 
