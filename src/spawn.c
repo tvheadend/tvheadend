99ca0081c2acd8a6b14ef5e779b3bd84f72e06c7
Jaroslav Kysela <perex@perex.cz>
2015-10-29 19:49:17 +0100

    fix spawn_parse_args() - for latest change

--- src/spawn.c
+++ src/spawn.c
@@ -361,8 +361,6 @@ spawn_parse_args(char ***argv, int argc, const char *cmd, const char **replace)
       s++;
     f = s;
     while (*s && *s != ' ') {
-      while (*s && *s != ' ' && *s != '\\')
-        s++;
       if (*s == '\\') {
         l = *(s + 1);
         if (l == 'b')
@@ -377,11 +375,16 @@ spawn_parse_args(char ***argv, int argc, const char *cmd, const char **replace)
           l = '\t';
         else
           l = 0;
-        if (l)
+        if (l) {
           *s++ = l;
-        memmove(s, s + 1, strlen(s) - 1);
-        if (*s)
-          s++;
+          memmove(s, s + 1, strlen(s));
+        } else {
+          memmove(s, s + 1, strlen(s));
+          if (*s)
+            s++;
+        }
+      } else {
+        s++;
       }
     }
     if (f != s) {
