7508b4c8cd3e24e9077c73d5ba040ae50dad0762
Mark Clarkstone <hello@markclarkstone.co.uk>
2016-02-19 18:08:39 +0000

    Tooltips: And another..

--- src/access.c
+++ src/access.c
@@ -1561,7 +1561,7 @@ const idclass_t access_entry_class = {
       .id       = "htsp_anonymize",
       .name     = N_("Anonymize HTSP access"),
       .desc     = N_("Do not send any stream specific information to "
-                     "the HTSP client like signal strength, input source"
+                     "the HTSP client like signal strength, input source "
                      "etc."),
       .off      = offsetof(access_entry_t, ae_htsp_anonymize),
       .opts     = PO_ADVANCED | PO_HIDDEN,
