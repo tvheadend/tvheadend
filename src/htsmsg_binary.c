e598efc4a0d1aa05c053f1a4e7ad32569319b235
Jaroslav Kysela <perex@perex.cz>
2016-06-20 10:31:42 +0200

    build fix for previous commit

--- src/htsmsg_binary.c
+++ src/htsmsg_binary.c
@@ -147,7 +147,7 @@ htsmsg_binary_deserialize(void *data, size_t len, const void *buf)
   htsmsg_t *msg = htsmsg_create_map();
   int r = htsmsg_binary_des0(msg, data, len);
   if (r < 0) {
-    free(buf);
+    free((void *)buf);
     htsmsg_destroy(msg);
     return NULL;
   }
@@ -158,7 +158,7 @@ htsmsg_binary_deserialize(void *data, size_t len, const void *buf)
     memoryinfo_append(&htsmsg_memoryinfo, len);
 #endif
   } else {
-    free(buf);
+    free((void *)buf);
   }
   return msg;
 }
