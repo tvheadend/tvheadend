542b98caecc3dd3ddedb25554881424ba278653f
Christian Karrié Webdevelopement <ckw@christian-lenovo>
2016-08-18 10:46:40 +0200

    python pep8 updates, additional functions

--- lib/py/tvh/log.py
+++ lib/py/tvh/log.py
@@ -23,45 +23,45 @@ Some very basic logging routines
 #
 DEBUG_LVL = None
 
-def debug_init ( lvl = None ):
-  global DEBUG_LVL
-  DEBUG_LVL = lvl
+
+def debug_init(lvl=None):
+    global DEBUG_LVL
+    DEBUG_LVL = lvl
+
 
 #
 # Output message
 #
-def out ( pre, msg, **dargs ):
-  import sys
-  import datetime, pprint
-  now = datetime.datetime.now()
-  if 'pretty' in dargs and dargs['pretty']:
-    ind = 2
-    if 'indent' in dargs: ind = dargs['indent']
-    msg = pprint.pformat(msg, indent=ind, width=70)
-  out = '%s %-6s: %s\n' % (now.strftime('%F %T'), pre, msg)
-  sys.stderr.write(out)
+def out(pre, msg, **dargs):
+    import sys
+    import datetime, pprint
+    now = datetime.datetime.now()
+    if 'pretty' in dargs and dargs['pretty']:
+        ind = 2
+        if 'indent' in dargs: ind = dargs['indent']
+        msg = pprint.pformat(msg, indent=ind, width=70)
+    out = '%s %-6s: %s\n' % (now.strftime('%F %T'), pre, msg)
+    sys.stderr.write(out)
+
 
 #
 # Debug
 #
-def debug ( msg, lvl=1, **dargs ):
-  if DEBUG_LVL and lvl <= DEBUG_LVL:
-    out('DEBUG', msg, **dargs)
+def debug(msg, lvl=1, **dargs):
+    if DEBUG_LVL and lvl <= DEBUG_LVL:
+        out('DEBUG', msg, **dargs)
+
 
 #
 # Info
 #
-def info ( msg, **dargs ):
-  out('INFO', msg, **dargs)
+def info(msg, **dargs):
+    out('INFO', msg, **dargs)
+
 
 #
 # Error
 #
-def error ( msg, **dargs ):
-  out('ERROR', msg, **dargs)
+def error(msg, **dargs):
+    out('ERROR', msg, **dargs)
 
-# ############################################################################
-# Editor Configuration
-#
-# vim:sts=2:ts=2:sw=2:et
-# ############################################################################
