d29e2cc0d1ec5eb906166f558850836b95f818bf
Pablo <trujulu@gmail.com>
2017-03-28 14:17:46 +0200

    Update Copyright year (2017)

--- src/webui/extjs.c
+++ src/webui/extjs.c
@@ -197,7 +197,7 @@ page_about(http_connection_t *hc, const char *remain, void *opaque)
 
   htsbuf_qprintf(hq, "<center class=\"about-tab\">\n\
 <div class=\"about-title\">HTS Tvheadend %s</div>\n\
-<p>&copy; 2006 - 2016 Andreas \303\226man, Jaroslav Kysela, Adam Sutton, et al.</p>\n\
+<p>&copy; 2006 - 2017 Andreas \303\226man, Jaroslav Kysela, Adam Sutton, et al.</p>\n\
 <p><img class=\"logobig\" src=\"static/img/logobig.png\"></p>\n\
 <p><a href=\"https://tvheadend.org\">https://tvheadend.org</a></p>\n",
     tvheadend_version);
