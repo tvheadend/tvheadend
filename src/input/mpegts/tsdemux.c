c8d961adf7c571b0f64646d8015a350fce57eb44
Jaroslav Kysela <perex@perex.cz>
2017-09-11 17:16:41 +0200

    tsremux: parse always HBBTV info

--- src/input/mpegts/tsdemux.c
+++ src/input/mpegts/tsdemux.c
@@ -189,16 +189,16 @@ ts_recv_packet0
         SCT_ISAUDIO(st->es_type))
       ts_recv_pcr_audio(t, st, tsb2 + off, 188 - off);
 
+    if (st->es_type == SCT_HBBTV) {
+      dvb_table_parse(&st->es_psi, "ts", tsb2, 188, 1, 0, ts_recv_hbbtv_cb);
+      continue;
+    }
+
     if (!streaming_pad_probe_type(&t->s_streaming_pad, SMT_PACKET))
       continue;
 
     if (st->es_type == SCT_CA || st->es_type == SCT_CAT)
       continue;
-
-    if (st->es_type == SCT_HBBTV) {
-      dvb_table_parse(&st->es_psi, "ts", tsb2, 188, 1, 0, ts_recv_hbbtv_cb);
-      continue;
-    }
 
     if (off <= 188 && t->s_status == SERVICE_RUNNING)
       parse_mpeg_ts((service_t*)t, st, tsb2 + off, 188 - off, pusi, error);
