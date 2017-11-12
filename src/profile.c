6b42f0c5061363caad22c0518085ef23854043df
Jaroslav Kysela <perex@perex.cz>
2017-11-12 18:40:40 +0100

    profile: print an error when the profile is problematic, issue #4721

--- src/profile.c
+++ src/profile.c
@@ -540,8 +540,11 @@ profile_find_by_list
   } else {
     res = pro;
   }
-  if (!res)
+  if (!res) {
     res = profile_find_by_name((sflags & SUBSCRIPTION_HTSP) ? "htsp" : NULL, NULL);
+    if (!profile_verify(res, flags))
+      tvherror(LS_PROFILE, "unable to select a working profile (asked '%s' alt '%s')", name, alt);
+  }
   return res;
 }
 
