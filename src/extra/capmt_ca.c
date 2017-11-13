ebe99da954f17e36c9b3621202e78e2c97cbd8ea
Jaroslav Kysela <perex@perex.cz>
2017-11-13 10:03:37 +0100

    capmt_ca: missing typedef

--- src/extra/capmt_ca.c
+++ src/extra/capmt_ca.c
@@ -47,10 +47,10 @@
 #include <errno.h>
 
 #ifndef CA_SET_PID /* removed in kernel 4.14 */
-struct ca_pid {
+typedef struct ca_pid {
   unsigned int pid;
   int index;		/* -1 == disable */
-};
+} ca_pid_t;
 #define CA_SET_PID _IOW('o', 135, struct ca_pid)
 #endif
 
