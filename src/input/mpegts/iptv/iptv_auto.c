5154f35b502c082e1340200499fb168435d82590
Jaroslav Kysela <perex@perex.cz>
2017-12-03 12:38:08 +0100

    IPTV auto network: fix uninitalized variable

--- src/input/mpegts/iptv/iptv_auto.c
+++ src/input/mpegts/iptv/iptv_auto.c
@@ -209,6 +209,7 @@ skip_url:
     im = (iptv_mux_t *)mm;
     if (strcmp(im->mm_iptv_url ?: "", url) == 0) {
       im->im_delete_flag = 0;
+      change = 0;
       if (strcmp(im->mm_iptv_svcname ?: "", name)) {
         free(im->mm_iptv_svcname);
         im->mm_iptv_svcname = strdup(name);
