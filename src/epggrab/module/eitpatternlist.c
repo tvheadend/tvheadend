715a4a84439c7a71063053ff5ec120fc27bdc683
Jim Hague <jim.hague@acm.org>
2017-12-27 14:55:02 +0000

    eit: allow scraper regexes to be marked as filters (#4818)
    Introduce an extended scraper regex syntax; as well as a string, a regex
    can be specified as a map. The map must have an entry "pattern" with the
    regex pattern. It may also have an entry "filter", with a numeric value.
    If this value is not 0, the regular expression is a filter.
    
    If a filter regular expression matches the input text, the result of the
    match replaces the input text, and processing continues from the next
    regular expression with that new input text.
    
    Issue: 4818

--- src/epggrab/module/eitpatternlist.c
+++ src/epggrab/module/eitpatternlist.c
@@ -22,25 +22,36 @@
 #include "eitpatternlist.h"
 #include "htsmsg.h"
 
+#define MAX_TEXT_LEN    2048
+
 void eit_pattern_compile_list ( eit_pattern_list_t *list, htsmsg_t *l, int flags )
 {
   eit_pattern_t *pattern;
   htsmsg_field_t *f;
   const char *s;
+  int filter;
 
   TAILQ_INIT(list);
   if (!l) return;
   HTSMSG_FOREACH(f, l) {
     s = htsmsg_field_get_str(f);
-    if (s == NULL) continue;
+    filter = 0;
+    if (s == NULL) {
+      htsmsg_t *m = htsmsg_field_get_map(f);
+      if (m == NULL) continue;
+      s = htsmsg_get_str(m, "pattern");
+      if (s == NULL) continue;
+      filter = htsmsg_get_bool_or_default(m, "filter", 0);
+    }
     pattern = calloc(1, sizeof(eit_pattern_t));
     pattern->text = strdup(s);
+    pattern->filter = filter;
     if (regex_compile(&pattern->compiled, pattern->text, flags, LS_EPGGRAB)) {
       tvhwarn(LS_EPGGRAB, "error compiling pattern \"%s\"", pattern->text);
       free(pattern->text);
       free(pattern);
     } else {
-      tvhtrace(LS_EPGGRAB, "compiled pattern \"%s\"", pattern->text);
+      tvhtrace(LS_EPGGRAB, "compiled pattern \"%s\", filter %d", pattern->text, pattern->filter);
       TAILQ_INSERT_TAIL(list, pattern, p_links);
     }
   }
@@ -74,7 +85,8 @@ static void rtrim(char *buf)
 void *eit_pattern_apply_list(char *buf, size_t size_buf, const char *text, eit_pattern_list_t *l)
 {
   eit_pattern_t *p;
-  char matchbuf[2048];
+  char textbuf[MAX_TEXT_LEN];
+  char matchbuf[MAX_TEXT_LEN];
   int matchno;
 
   assert(buf);
@@ -93,7 +105,13 @@ void *eit_pattern_apply_list(char *buf, size_t size_buf, const char *text, eit_p
         strncat(buf, matchbuf, size_buf - len - 1);
       }
       rtrim(buf);
-      tvhtrace(LS_EPGGRAB,"  pattern \"%s\" matches with '%s'", p->text, buf);
+      tvhtrace(LS_EPGGRAB,"  pattern \"%s\" matches '%s' from '%s'", p->text, buf, text);
+      if (p->filter) {
+        strncpy(textbuf, buf, MAX_TEXT_LEN - 1);
+        textbuf[MAX_TEXT_LEN - 1] = '\0';
+        text = textbuf;
+        continue;
+      }
       return buf;
     }
   return NULL;
