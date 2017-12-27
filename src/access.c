94ce31d7735023b66a63a14e4951958664248717
Jaroslav Kysela <perex@perex.cz>
2017-12-27 09:30:35 +0100

    access: use bin2hex() instead own bin to hex converter

--- src/access.c
+++ src/access.c
@@ -129,7 +129,6 @@ access_ticket_create(const char *resource, access_t *a)
   char id[41];
   uint_fast32_t i;
   access_ticket_t *at;
-  static const char hex_string[16] = "0123456789ABCDEF";
 
   assert(a);
 
@@ -146,14 +145,8 @@ access_ticket_create(const char *resource, access_t *a)
 
   at = calloc(1, sizeof(access_ticket_t));
 
-  uuid_random(buf, 20);
-
-  //convert to hexstring
-  for (i=0; i < sizeof(buf); i++){
-    id[i*2] = hex_string[((buf[i] >> 4) & 0xF)];
-    id[(i*2)+1] = hex_string[(buf[i]) & 0x0F];
-  }
-  id[40] = '\0';
+  uuid_random(buf, sizeof(buf));
+  bin2hex(id, sizeof(id), buf, sizeof(buf));
 
   at->at_id = strdup(id);
   at->at_resource = strdup(resource);
