5035e2510ed63da1f05f8779f7a575473ec732a7
pablozg <pablozg@gmail.com>
2018-04-25 11:26:23 +0200

    Add syntax check and minor fixes

--- src/webui/static/app/epg.js
+++ src/webui/static/app/epg.js
@@ -506,10 +506,11 @@ tvheadend.epg = function() {
 
         if (value) {
           var dt = new Date(value);
-          if (tvheadend.date_mask.length > 1){
+          if(/([%][MmsSyYdhq]+)/.test(tvheadend.date_mask)){
             return tvheadend.toCustomDate(dt,tvheadend.date_mask);
           }else{
-            return tvheadend.toCustomDate(dt,'%ddd, %MMMM %dd, %hh:%mm');
+            var options = {weekday: 'short', day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false};
+            return dt.toLocaleString(tvheadend.toLocaleFormat(), options);
           }
         }
         return "";
