c20a90638badfff1a25634bb26baf7728c124390
pablozg <pablozg@gmail.com>
2018-06-08 11:30:09 +0200

    webui: Added support for Kodi label formatting in dvr/epg as optional

--- src/webui/static/app/dvr.js
+++ src/webui/static/app/dvr.js
@@ -5,6 +5,22 @@
 /**
  *
  */
+
+tvheadend.labelFormattingParser = function(description) {
+    if (tvheadend.label_formatting){
+        return description.replace(/\[COLOR\s(.*?)\]/g, '<font style="color:$1">')
+	                   .replace(/\[\/COLOR\]/g, '<\/font>')
+	                   .replace(/\[B\]/g, '<b>')
+	                   .replace(/\[\/B\]/g, '<\/b>')
+	                   .replace(/\[I\]/g, '<i>')
+	                   .replace(/\[CR\]/g, '<br>')
+	                   .replace(/\[\/I\]/g, '<\/i>')
+	                   .replace(/\[UPPERCASE\](.*)\[\/UPPERCASE\]/g, function(match, group) {return group.toUpperCase();})
+	                   .replace(/\[LOWERCASE\](.*)\[\/LOWERCASE\]/g, function(match, group) {return group.toLowerCase();})
+	                   .replace(/\[CAPITALIZE\](.*)\[\/CAPITALIZE\]/g, function(match, group) {return group.split(/\s/).map(w => w[0].toUpperCase()  w.slice(1)).join(' ');});
+    }else return description;
+};
+
 tvheadend.dvrDetails = function(uuid) {
 
     function showit(d) {
@@ -79,7 +95,7 @@ tvheadend.dvrDetails = function(uuid) {
         if (summary && (!subtitle || subtitle != summary))
             content += '<div class="x-epg-summary">' + summary + '</div>';
         if (desc) {
-            content += '<div class="x-epg-desc">' + desc + '</div>';
+            content += '<div class="x-epg-desc">' + tvheadend.labelFormattingParser(desc) + '</div>';
             content += '<hr class="x-epg-hr"/>';
         }
         content += tvheadend.getDisplayCredits(credits);
