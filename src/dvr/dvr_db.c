de34110fca4ca9c8e894845715e7a63b855aabd6
E.Smith <31170571+azlm8t@users.noreply.github.com>
2018-09-26 00:19:07 +0100

    dvr: Allow artwork to be set via idnode/save.

--- src/dvr/dvr_db.c
+++ src/dvr/dvr_db.c
@@ -3935,7 +3935,7 @@ const idclass_t dvr_entry_class = {
       .desc     = N_("Episode image."),
       .get      = dvr_entry_class_image_url_get_as_property,
       .off      = offsetof(dvr_entry_t, de_image),
-      .opts     = PO_HIDDEN | PO_RDONLY,
+      .opts     = PO_HIDDEN,
     },
     {
       .type     = PT_STR,
@@ -3943,7 +3943,7 @@ const idclass_t dvr_entry_class = {
       .name     = N_("Fanart image"),
       .desc     = N_("Fanart image."),
       .off      = offsetof(dvr_entry_t, de_fanart_image),
-      .opts     = PO_HIDDEN | PO_RDONLY,
+      .opts     = PO_HIDDEN,
     },
     {
       .type     = PT_LANGSTR,
