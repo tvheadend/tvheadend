718fdbc9fa5a75486f15e75a7284ab47052c88f4
E.Smith <31170571+azlm8t@users.noreply.github.com>
2018-09-29 12:38:37 +0100

    ui: Add TMDB attribution strings and logos.
    The logo is taken from:
    https://www.themoviedb.org/about/logos-attribution
    and resized with:
    convert -resize 32x

--- src/webui/extjs.c
+++ src/webui/extjs.c
@@ -207,8 +207,9 @@ page_about(http_connection_t *hc, const char *remain, void *opaque)
 %s <a target=\"_blank\" href=\"http://www.famfamfam.com/lab/icons/silk/\">\
 FamFamFam</a>, "\
 "<a target=\"_blank\" href=\"https://www.google.com/get/noto/help/emoji/\">Google Noto Color Emoji</a> "\
-"<a target=\"_blank\" href=\"https://raw.githubusercontent.com/googlei18n/noto-emoji/master/LICENSE\">(Apache Licence v2.0)</a>\n\
-</p>\n",
+"<a target=\"_blank\" href=\"https://raw.githubusercontent.com/googlei18n/noto-emoji/master/LICENSE\">(Apache Licence v2.0)</a>.\n"\
+"\nThis product uses the TMDB API but is not endorsed or certified by <a target=\"_blank\" href=\"https://www.themoviedb.org\">TMDb</a> <img src=\"static/img/tmdb.png\">\n"\
+"</p>\n",
     tvh_gettext_lang(lang, N_("Based on software from")),
     tvh_gettext_lang(lang, N_("Icons from")));
 
