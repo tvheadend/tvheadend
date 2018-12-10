eadecbf8b32118e9272582e6d95c12825c21b246
E.Smith <31170571+azlm8t@users.noreply.github.com>
2018-12-10 00:15:15 +0000

    xmltv: Ensure we pass through lang when adding genres from xmltv (#3753)

--- src/epggrab/module/xmltv.c
+++ src/epggrab/module/xmltv.c
@@ -535,18 +535,23 @@ _xmltv_map_category_to_epg_genre(const char* xmltv_category)
 static epg_genre_list_t
 *_xmltv_parse_categories ( htsmsg_t *tags )
 {
-  htsmsg_t *e;
+  htsmsg_t *e, *attrib;
   htsmsg_field_t *f;
   epg_genre_list_t *egl = NULL;
+  const char *lang;
   HTSMSG_FOREACH(f, tags) {
     if (!strcmp(htsmsg_field_name(f), "category") && (e = htsmsg_get_map_by_field(f))) {
       if (!egl) egl = calloc(1, sizeof(epg_genre_list_t));
       const char *xmltv_category = htsmsg_get_str(e, "cdata");
       /* Attempt to map xmltv category name to the genre, e.g.,
-       * "Opera" becomes "Musical / Opera".
+       * "Opera" becomes "Musical / Opera", "Ocio y Aficiones" becomes
+       * "Leisure hobbies".
        */
       const char *epg_genre = _xmltv_map_category_to_epg_genre(xmltv_category);
-      epg_genre_list_add_by_str(egl, epg_genre, NULL);
+      lang = NULL;
+      if ((attrib = htsmsg_get_map(e, "attrib")))
+        lang = htsmsg_get_str(attrib, "lang");
+      epg_genre_list_add_by_str(egl, epg_genre, lang);
     }
   }
   return egl;
