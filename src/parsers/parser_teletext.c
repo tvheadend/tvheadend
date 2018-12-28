b17dcf91490c38df678472bef3a117b4c6e2996c
Jaroslav Kysela <perex@perex.cz>
2018-12-28 21:21:36 +0100

    teletext: fix the subtitle parser (wrong SCT_ type match), issue #5422

--- src/parsers/parser_teletext.c
+++ src/parsers/parser_teletext.c
@@ -845,9 +845,9 @@ tt_subtitle_deliver(parser_t *t, parser_es_t *parent, tt_mag_t *ttm)
 
   TAILQ_FOREACH(es, &t->prs_components.set_filter, es_filter_link) {
     st = (parser_es_t *)es;
-    if (st->es_type != SCT_TELETEXT) continue;
+    if (st->es_type != SCT_TEXTSUB) continue;
     if (parent->es_pid == st->es_parent_pid &&
-      ttm->ttm_curpage == st->es_pid -  PID_TELETEXT_BASE) {
+        ttm->ttm_curpage == st->es_pid - PID_TELETEXT_BASE) {
       if (extract_subtitle(t, st, ttm, ttm->ttm_current_pts))
         ttm->ttm_current_pts++; // Avoid duplicate (non-monotonic) PTS
     }
