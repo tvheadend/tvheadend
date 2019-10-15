76626a94646223f8e73c2168fa4b7a28c5bb8046
Jaroslav Kysela <perex@perex.cz>
2019-10-15 09:11:06 +0200

    tvhcsa: shift the standard headers to top

--- src/descrambler/tvhcsa.c
+++ src/descrambler/tvhcsa.c
@@ -17,6 +17,10 @@
  */
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <assert.h>
+
 #include "tvhcsa.h"
 #include "input.h"
 #include "input/mpegts/tsdemux.h"
@@ -25,9 +29,6 @@
 #include "descrambler/algo/libaes128dec.h"
 #include "descrambler/algo/libdesdec.h"
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <assert.h>
 
 static void
 tvhcsa_empty_flush
