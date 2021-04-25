58898e08895f9329e4c1882b4ae4de3d137f649f
Jumper78 <52802286+Jumper78@users.noreply.github.com>
2021-04-25 17:56:49 +0200

    Update muxer_pass.c

--- src/muxer/muxer_pass.c
+++ src/muxer/muxer_pass.c
@@ -648,7 +648,7 @@ pass_muxer_write_ts(muxer_t *m, pktbuf_t *pb)
       pid = (tsb[1] & 0x1f) << 8 | tsb[2];
       l = mpegts_word_count(tsb, len2, 0x001FFF00);
 
-      tvhwarn(LS_PASS, "PID: %i", pid);
+      //tvhwarn(LS_PASS, "PID out of if: %i", pid);
 
       /* Process */
       if ( (pm->m_config.u.pass.m_rewrite_pat && pid == DVB_PAT_PID) ||
@@ -656,6 +656,8 @@ pass_muxer_write_ts(muxer_t *m, pktbuf_t *pb)
            (pm->pm_rewrite_sdt && pid == DVB_SDT_PID) ||
            (pm->pm_rewrite_nit && pid == DVB_NIT_PID) ||
            (pm->pm_rewrite_eit && pid == DVB_EIT_PID) ) {
+
+        tvhwarn(LS_PASS, "  PID in if: %i", pid);
 
         /* Flush */
         if (len)
@@ -672,11 +674,13 @@ pass_muxer_write_ts(muxer_t *m, pktbuf_t *pb)
 
         /* SDT */
         } else if (pid == DVB_SDT_PID) {
-        
+          tvhwarn(LS_PASS, "    DVB_SDT_PID");
+          
           dvb_table_parse(&pm->pm_sdt, "-", tsb, l, 1, 0, pass_muxer_sdt_cb);
 
         /* NIT */
         } else if (pid == DVB_NIT_PID) {
+          tvhwarn(LS_PASS, "    DVB_NIT_PID");
         
           dvb_table_parse(&pm->pm_nit, "-", tsb, l, 1, 0, pass_muxer_nit_cb);
 
