28a3d55f9f67caa0c161b04ec45c300d8064e01f
Flole <flole@flole.de>
2022-06-18 00:07:09 +0200

    Cloudsmith upload

---
+++ support/cloudsmith.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#
+# Entry point for the Cloudsmith upload system
+#
+# (c) Flole 2022. All rights reserved.
+#
+#
+
+set -eu
+
+TARGET=""
+OSPREFIX=""
+OS=""
+FILE=""
+
+while getopts "t:p:f:" OPTION
+do
+  case $OPTION in
+      t)
+          TARGET="$OPTARG"
+          ;;
+      p)
+          OSPREFIX="$OPTARG"
+          ;;
+      f)
+          FILE="$OPTARG"
+          ;;
+  esac
+done
+
+if [[ -z $FILE ]]; then
+    echo "No file specified"
+    exit 1
+fi
+
+if [[ -z $TARGET ]]; then
+    source Autobuild/identify-os.sh
+    TARGET="$DISTRO"
+fi
+
+case $OSPREFIX$TARGET in
+    bookworm|bullseye|buster|sid|stretch|jessie)
+        OS="debian";;
+    bionic|focal|jammy|trusty|xenial)
+        OS="ubuntu";;
+    raspiosbullseye|raspiosbuster|raspiosjessieraspiosstretch)
+        OS="raspbian";;
+    *) echo "OS $TARGET could not be recognized" && exit 1;;
+esac
+
+pip install --upgrade cloudsmith-cli
+
+FILEARRAY=($FILE)
+
+for package in "${FILEARRAY[@]}"; do
+    cloudsmith push deb "tvheadend/tvheadend/$OS/$TARGET" $package
+done
