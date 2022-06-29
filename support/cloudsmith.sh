1230e6d908d16d21ba1a55fff93b4a09b4179536
Flole998 <Flole998@users.noreply.github.com>
2022-06-29 03:16:13 +0200

    Attempt update to python 3.7 to fix errors

--- support/cloudsmith.sh
+++ support/cloudsmith.sh
@@ -52,6 +52,8 @@ export LC_ALL=C.UTF-8
 export LANG=C.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
+apt install -y python3.7 || true
+
 apt install -y python3-pip || apt install -y python-pip
 
 pip3 install --upgrade cloudsmith-cli || pip install --upgrade cloudsmith-cli || pip2 install --upgrade cloudsmith-cli
