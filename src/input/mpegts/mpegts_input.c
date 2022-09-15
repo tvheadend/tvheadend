86f3617c8972c5362e51cee7d34cc2d69d799126
Flole998 <Flole998@users.noreply.github.com>
2022-09-15 02:31:24 +0200

    Fix crash when mpegts_service_refresh tries to open the CAT again

--- src/input/mpegts/mpegts_input.c
+++ src/input/mpegts/mpegts_input.c
@@ -821,7 +821,8 @@ void
 mpegts_input_open_cat_monitor
   ( mpegts_mux_t *mm, mpegts_service_t *s )
 {
-  assert(s->s_cat_mon == NULL);
+  if (s->s_cat_mon)
+    mpegts_table_destroy(s_cat_mon);
   s->s_cat_mon =
     mpegts_table_add(mm, DVB_CAT_BASE, DVB_CAT_MASK,
                      mpegts_input_cat_pass_callback, s, "cat",
