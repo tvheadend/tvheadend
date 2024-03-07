ba3b5e56f2f25efb8298a12b5118843de053813d
Flole998 <Flole998@users.noreply.github.com>
2024-03-07 16:18:42 +0100

    Create special tvheadend-armv6l and tvheadend-dbg-armv6l packages
    Fixes: #1665

--- Autobuild/armv6l.sh
+++ Autobuild/armv6l.sh
@@ -1,1 +1,4 @@
 AUTOBUILD_CONFIGURE_EXTRA="${AUTOBUILD_CONFIGURE_EXTRA:-} --arch=armel"
+sed -i -E '/-armv6l$/! s/^Package: (.*)(-armv6l)*$/Package: \1-armv6l/g' debian/control
+sed -i -E 's/^Depends: tvheadend/Depends: tvheadend-armv6l/g' debian/control
+sed -i -E '/armv6l/! s/package=tvheadend([^ ]*)/package=tvheadend\1-armv6l/g' debian/rules
