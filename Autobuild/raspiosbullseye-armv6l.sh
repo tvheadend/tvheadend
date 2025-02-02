81fa396a89204f0788a922c38a6d75f45b1df1fa
Flole <Flole998@users.noreply.github.com>
2025-02-02 16:28:43 +0000

    Fix CI Builds 1/2
    Update run-on-arch to v3
    
    This should finally fix the segfaults
    
    Fix x265 builds
    
    Fix cloudsmith uploads for Fedora
    
    Ensure new x265 flags are used
    
    Fix native builds
    
    Update libx265

--- Autobuild/raspiosbullseye-armv6l.sh
+++ Autobuild/raspiosbullseye-armv6l.sh
@@ -1,2 +1,3 @@
 source Autobuild/armv6l.sh
+EXTRA_X265_CMAKE_OPTS="${EXTRA_X265_CMAKE_OPTS:-} -DCROSS_COMPILE_ARM=0"
 source Autobuild/raspiosbullseye.sh
--- Autobuild/xenial-armv7l.sh
+++ Autobuild/xenial-armv7l.sh
@@ -1,2 +1,3 @@
+AUTOBUILD_CONFIGURE_EXTRA="${AUTOBUILD_CONFIGURE_EXTRA:-} --disable-libx265_static --disable-libx265"
 source Autobuild/armv7l.sh
 source Autobuild/xenial.sh
