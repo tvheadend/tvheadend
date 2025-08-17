4bf4f7d87ccf343c3bbe9cb65eb5a6133d168044
FingerlessGloves <me@FingerlessGloves.me>
2025-08-18 00:08:04 +0100

    Add EL9 and EL10 support for both x86_64 and aarch64

--- Autobuild/identify-os.sh
+++ Autobuild/identify-os.sh
@@ -4,18 +4,24 @@ UNAME=$(uname | tr "[:upper:]" "[:lower:]")
 if [ "$UNAME" == "linux" ]; then
     # If available, use LSB to identify distribution
     if [ -x "$(command -v lsb_release)" ]; then
-        export DISTRO=$(lsb_release -c | cut -d: -f2 | sed s/'^\t'//)
+        export DISTRO=$(lsb_release -i | cut -d: -f2 | sed 's/^[[:space:]]*//' | tr "[:upper:]" "[:lower:]")
+        export CODENAME=$(lsb_release -c | cut -d: -f2 | sed s/'^\t'//)
         echo "OS identified using lsb_release command"
     elif [ -f /etc/lsb-release ]; then
-         export DISTRO=$(awk -F= '/^DISTRIB_CODENAME/{print $2}' /etc/lsb-release)
+         export DISTRO=$(awk -F= '/^DISTRIB_ID/{print $2}' /etc/lsb-release | tr "[:upper:]" "[:lower:]")
+         export CODENAME=$(awk -F= '/^DISTRIB_CODENAME/{print $2}' /etc/lsb-release)
         echo "OS identified using lsb_release file"
     # Otherwise, use release info file
     elif [ -f /etc/os-release ]; then
-        export DISTRO=$(awk -F= '/^VERSION_CODENAME/{print $2}' /etc/os-release)
-        # Fedora uses VERSION_ID
-        if [ "$DISTRO" == '""' ]; then
-            export DISTRO=$(awk -F= '/^VERSION_ID/{print $2}' /etc/os-release)
+        # Detect EL based
+        ID_LIKE=$(awk -F= '/^ID_LIKE=/{print $2}' /etc/os-release | tr -d '"')
+        if [[ "$ID_LIKE" == *rhel* ]]; then
+            export DISTRO="rhel"
+        else # default to ID
+            export DISTRO=$(awk -F= '/^ID=/{print $2}' /etc/os-release | tr -d '"')
         fi
+        export CODENAME=$(awk -F= '/^VERSION_CODENAME=/{print $2}' /etc/os-release)
+        export VERSION=$(awk -F= '/^VERSION_ID=/{print $2}' /etc/os-release | tr -d '"')
         echo "OS identified using os-release file"
     else
         export DISTRO=$(ls -d /etc/[A-Za-z]*[_-][rv]e[lr]* | grep -v "lsb" | cut -d'/' -f3 | cut -d'-' -f1 | cut -d'_' -f1)
