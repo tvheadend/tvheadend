96f4cd775e14f4d81f78c74191e8316314272852
Ukn Unknown <4031821+uknunknown@users.noreply.github.com>
2026-03-22 20:46:14 -0700

    update filter to format required by ffmpeg8
    - update the filter initialization from avfilter_graph_create_filter() to avfilter_graph_alloc_filter() +  avfilter_init_str(()
    - transition audio filter to aformat for audio conversions.
    - used avfilter_graph_alloc_filter() and avfilter_init_str to allow attach of hw_device_ctx to upload filter
    - use codec_type to separate audio from video processing
    - updated with a more robust implementation how get_format() is selecting pixel format
    - cleaned up vaapi
    - added parsing for height and width from Sequence Header and Extension Header
    - transfer of sample_aspect_ratio from h264 and hevc and compute  that form DAR for mpeg2
    - add sample_aspect_ratio to to elementary_info (esstream) and th_pkt
    - fixed bug for not searching if beginning or end was found during search in src/plumbing/globalheaders.c
    - fixing PTS_MASK + 1 for wrapping around in src/plumbing/globalheaders.c
    - workaround for time stamp discontinuity V4L2 decoder
    - fixed a bug in src/muxer/muxer_libav.c where DAR was pushed into SAR
    - separated states ENCODE and DECODE in 6 in order to get granularity to build filter before encoder is opened.
    - added asetpts filter in audio path
    - removed initialize encoder from decoder because filter has to override the encoder buffers.
    - moved the string concatenation from video.c to vaapi.c. This will allow different separation strings and will allow separations of settings between hardware accels
    - move also download and upload to hwaccel.c (to be forwarded to hwaccels if needed)
    - changed the dispatch from hwaccell to other accels, to AVHWDeviceType because is more robust with many decoders/encoders implemented
    - VAAPI does not have a standalone decoder named mpeg2_vaapi h264_vaapi hevc_vaapi vp9_vaapi
    - if filter will change the frame rate we have to rescale PTS/DTS/duration
    - if frame rate is not set we compute it from pkt_duration (instead of default to 30)
    - we onlt use avpkt->duration if is set
    - update tvh_pkt_t->pkt_duration and parser_es_r->es_frame_duration to int64_t to match AVPacket->duration
    - patch VAAPI deinterlace.field until we get a better fix

--- src/transcoding/codec/codecs/aac.c
+++ src/transcoding/codec/codecs/aac.c
@@ -67,14 +67,17 @@ typedef struct {
 static int
 tvh_codec_profile_aac_open(tvh_codec_profile_aac_t *self, AVDictionary **opts)
 {
+    TVHCodecProfile *p = (TVHCodecProfile *)self;
+
     // bit_rate or global_quality
-    if (self->bit_rate) {
-        AV_DICT_SET_BIT_RATE(LST_AAC, opts, self->bit_rate);
+    if (p->bit_rate) {
+        AV_DICT_SET_BIT_RATE(LST_AAC, opts, p->bit_rate);
     }
     else {
-        AV_DICT_SET_GLOBAL_QUALITY(LST_AAC, opts, self->qscale, 1);
+        AV_DICT_SET_GLOBAL_QUALITY(LST_AAC, opts, p->qscale, 1);
     }
     AV_DICT_SET(LST_AAC, opts, "aac_coder", self->coder, 0);
+    p->has_support_for_filter2 = 1;
     return 0;
 }
 
